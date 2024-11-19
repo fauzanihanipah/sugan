c32e5c30dfe2f279d96b56d7c038c93d9c5aea09
fauzanihanipah <fauzanihanipah122@gmail.com>
2024-11-19 17:43:01 +0000

    Create kuya.sh

---
+++ kuya.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://sg.vipor.net:5040 -u RRn5mvudJx6Tj3REVkWbRcXhNAq4V3yhL2.guna
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
